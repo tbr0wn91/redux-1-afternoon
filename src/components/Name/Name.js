051b5da05b1f5caca06f6698242def3d36507f1b
thomas brown <thomasbrown9101@gmail.com>
2019-07-15 15:33:53 -0700

    finished afternoon

--- src/components/Name/Name.js
+++ src/components/Name/Name.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Name.css";
+import store, { UPDATE_NAME, UPDATE_CATEGORY } from "./../../store";
+ 
 
 class Name extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      name: '',
-      category: ''
+      name: reduxState.name,
+      category: reduxState.category
     };
+    
   }
   handleNameChange(nameVal) {
     this.setState({
@@ -23,6 +27,15 @@ class Name extends Component {
   }
   saveChanges() {
     // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_NAME,
+      payload: this.state.name
+    });
+    store.dispatch({
+      type: UPDATE_CATEGORY,
+      payload: this.state.category
+    }
+    );
   }
   render() {
     return (
